a3011817872b164295f256849784305562ac7c9e
ford-from-the-future <khalic44@gmail.com>
2024-03-15 16:56:37 -0500

    2nd

--- chess/common/Location.java
+++ chess/common/Location.java
@@ -17,5 +17,33 @@ public class Location {
         return rank;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((file == null) ? 0 : file.hashCode());
+        result = prime * result + ((rank == null) ? 0 : rank.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Location other = (Location) obj;
+        if (file != other.file)
+            return false;
+        if (rank == null) {
+            if (other.rank != null)
+                return false;
+        } else if (!rank.equals(other.rank))
+            return false;
+        return true;
+    }
+
     
 }
